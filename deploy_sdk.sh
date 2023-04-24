e41260f806c084f31d92d1f83c774a4d18ce9142
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2023-04-24 07:41:14 +0530

    deploy_sdk modified,deploy-yes

--- deploy_sdk.sh
+++ deploy_sdk.sh
@@ -15,36 +15,21 @@ DEPLOYMENT_IS_RUNNING=`echo "${BUILD_REPO_NAME}_${TRAVIS_BRANCH}" | tr '[:upper:
 export BASE_DOMAIN="froala-infra.com"
 export SDK_ENVIRONMENT=""
 export DEPLOYMENT_SERVER=""
-#docker-compose service
 SERVICE_NAME=""
-# container name --- will be used to identify the oldest deployment for this env
 CONTAINAER_NAME=""
 DB_CONTAINER_NAME=""
-# container-index --- will be used to identify the oldest deployment for this env
-# CONTAINER_NAME will be CONTAINER_NAME-INDEX
 CT_INDEX=0
-
 OLDEST_CONTAINER=""
 OLDEST_DATABASE_CONTAINER=""
-#
-# make sure we have ssh key from pipeline start
-#
 echo "${SSH_KEY}"  | base64 --decode > /tmp/sshkey.pem
 chmod 400 /tmp/sshkey.pem
-
-####
-
 export MAX_DEPLOYMENTS_NR=0
-#
-# find the max deployments alloowed per environment; it is defined in version.json file
-#
 function get_max_deployments_per_env(){
 
 local ENVIRONMENT=$1
 echo "getting max deployments for environment ${ENVIRONMENT}"
 MAX_DEPLOYMENTS_NR=`jq --arg sdkenvironment ${ENVIRONMENT}  '.[$sdkenvironment]' version.json | tr -d '"'`
 echo "detected max deployments: ${MAX_DEPLOYMENTS_NR}"
-#return $MAX_DEPLOYMENTS_NR
 }
 
 
@@ -53,10 +38,7 @@ local LW_REPO_NAME=$1
 local LW_SHORT_TRAVIS_BRANCH=$2
 local SDK_ENVIRONMENT=$3
 local DEPLOYMENT_SERVER=$4
-# get the index
-
 echo "searching for ${LW_REPO_NAME} depl..."
-# dont't fall into "About a minute ago "
 sleep 1
 
 RUNNING_DEPL=`ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker ps | grep -i ${LW_REPO_NAME}"`
@@ -65,7 +47,6 @@ echo "running depl var: ${RUNNING_DEPL}"
 echo "looking for ${LW_REPO_NAME} deployments"
 
 echo "getting indexes for oldest and latest deployed container"
-# build ssh cmd to get a list of running containers for this env
 DEPL='ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem '
 DEPL="${DEPL}  ${SSH_USER}@${DEPLOYMENT_SERVER} "
 REL=' " sudo docker ps | grep -i ' 
@@ -78,7 +59,6 @@ echo "show docker containers ssh cmd:  $DEPL"
 echo   ${DEPL}  | bash > file.txt
 echo "running conatiners: "
 cat file.txt
-# get those indexes  ; $NF always prints the last column
 CT_LOWER_INDEX=`cat file.txt | awk -F'-' '{print $NF }' | sort -nk1 | head -1`
 CT_HIGHER_INDEX=`cat file.txt | awk -F'-' '{print $NF }' | sort -nk1 | tail -1`
 
@@ -118,7 +98,6 @@ get_max_deployments_per_env $SDK_ENVIRONMENT
 echo "deploying on environment :${SDK_ENVIRONMENT}, on server ${DEPLOYMENT_SERVER}, max deployments: ${MAX_DEPLOYMENTS_NR}"
 
 export BASE_DOMAIN="froala-infra.com"
-# Issues with CN for certificates ; lenght must be max 64
 SHORT_REPO_NAME="${BUILD_REPO_NAME:0:17}"
 BRANCH_LENGHT=`echo ${TRAVIS_BRANCH} |awk '{print length}'`
 if [ ${BRANCH_LENGHT} -lt 18 ]; then 
@@ -134,26 +113,14 @@ SHORT_TRAVIS_BRANCH=`echo ${SHORT_TRAVIS_BRANCH} | sed -r 's/_//g'`
 echo " short branch name : ${SHORT_TRAVIS_BRANCH}"
 DEPLOYMENT_URL="${SHORT_REPO_NAME}-${SHORT_TRAVIS_BRANCH}.${SDK_ENVIRONMENT}.${BASE_DOMAIN}"
 echo " deployment URL: https://${DEPLOYMENT_URL}"
-
-#DEPLOYMENT_URL="${BUILD_REPO_NAME}-${TRAVIS_BRANCH}.${SDK_ENVIRONMENT}.${BASE_DOMAIN}"
-#echo " deployment URL: https://${DEPLOYMENT_URL}"
-
-#############
-# creating docker-compose file...
-#
 cp docker-compose.yml.template docker-compose.yml
-#manipulate to strictly verify depl on envs
 LW_REPO_NAME=`echo "${BUILD_REPO_NAME}" | tr '[:upper:]' '[:lower:]'`
-LW_REPO_NAME=`echo ${LW_REPO_NAME} | sed -r 's/_//g'`  #delete all _ from service name
-LW_REPO_NAME=`echo ${LW_REPO_NAME} | sed -r 's/-//g'`  #delete all - from service name
-LW_REPO_NAME=`echo ${LW_REPO_NAME} | sed -r 's/\.//g'`  #delete all . from service name
+LW_REPO_NAME=`echo ${LW_REPO_NAME} | sed -r 's/_//g'`
+LW_REPO_NAME=`echo ${LW_REPO_NAME} | sed -r 's/-//g'`
+LW_REPO_NAME=`echo ${LW_REPO_NAME} | sed -r 's/\.//g'`
 LW_SHORT_TRAVIS_BRANCH=`echo "${SHORT_TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
-#SERVICE_NAME=`echo "${BUILD_REPO_NAME}-${SHORT_TRAVIS_BRANCH}" | tr '[:upper:]' '[:lower:]'`
-
 SERVICE_NAME="${LW_REPO_NAME}-${LW_SHORT_TRAVIS_BRANCH}" 
-
 generate_container_name ${LW_REPO_NAME} ${LW_SHORT_TRAVIS_BRANCH} ${DEPLOYMENT_SERVER} ${DEPLOYMENT_SERVER} 
-
 CONTAINER_NAME="${LW_REPO_NAME}-${CT_INDEX}"
 DB_SERVICE_NAME="${DB_SERVICE_NAME}-${CT_INDEX}"
 DB_CONTAINER_NAME="${DB_SERVICE_NAME}"
@@ -176,23 +143,11 @@ sed -i "s/MysqlContainerName/${DB_CONTAINER_NAME}/g" docker-compose.yml
 
 cat docker-compose.yml
 
-######
-#  Redeploy or don't deploy if max deployments reached
-#
-#echo "${SSH_KEY}"  | base64 --decode > /tmp/sshkey.pem
-#chmod 400 /tmp/sshkey.pem
-#SHORT_SERVICE_NAME="${SERVICE_NAME:0:15}"
 LW_REPO_NAME_LENGTH=`echo ${LW_REPO_NAME} |awk '{print length}'`
 SHORT_SERVICE_NAME="${SERVICE_NAME:0:$LW_REPO_NAME_LENGTH}"
 echo "short service name: ${SHORT_SERVICE_NAME}"
 
-
-
 function deploy_service(){
-#####
-#deploy the container
-#
-# make sure docker-compose dir exists and is clean
 ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} "if [ -d /services/${SERVICE_NAME} ];  then sudo docker-compose -f /services/${SERVICE_NAME}/docker-compose.yml down; fi"
 ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} "if [ -d /services/${SERVICE_NAME} ];  then rm -rf /services/${SERVICE_NAME}; fi && mkdir /services/${SERVICE_NAME}"
 scp  -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem docker-compose.yml ${SSH_USER}@${DEPLOYMENT_SERVER}:/services/${SERVICE_NAME}/docker-compose.yml
@@ -202,13 +157,8 @@ sleep 10 && ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${
 
 echo "Docker-compose is in : /services/${SERVICE_NAME} "
 
-#############
-#
-# workaround for froala plugin to consume unpublished core library : deactivate plugin & replace files 
-#
 sleep 30
 ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker exec ${CONTAINER_NAME} wp core update --allow-root "
-##ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " 
 ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker exec ${CONTAINER_NAME} wp core install --allow-root --url=https://${DEPLOYMENT_URL} --admin_user=${WORD_USER} --admin_email=${WORD_EMAIL} --admin_password=${WORD_PASS} --title=${WORD_TITLE} "
 ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker exec ${CONTAINER_NAME} wp plugin deactivate froala --allow-root && sleep 5" 
 ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker exec ${CONTAINER_NAME} rm -rf /var/www/html/wp-content/plugins/froala/admin/css && sleep 5" 
@@ -221,12 +171,6 @@ ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_S
 
 echo "\n If no error above (cp related errors) then froala plugin is consuming the unpublished core library \n" 
 
-#############
-
-#############
-#
-# validate deployment
-#
 sleep 10
 RET_CODE=`curl -k -s -o /tmp/notimportant.txt -w "%{http_code}" https://${DEPLOYMENT_URL}`
 echo "validation code: $RET_CODE for  https://${DEPLOYMENT_URL}"
@@ -241,20 +185,13 @@ fi
 
 }  
 
-#using SHORT_SERVICE_NAME to redeploy the service
 REDEPLOYMENT=`ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker ps -a | grep -i "${DEPLOYMENT_IS_RUNNING}" | wc -l" `
 echo "${DEPLOYMENT_IS_RUNNING}"
 echo "checking if this PRD exists & do redeploy: ${REDEPLOYMENT}"
 if [ ${REDEPLOYMENT} -eq 1 ]; then 
-#	REDEPLOYMENT=true
 	echo "Redeploying service: ${SERVICE_NAME} ..."
-	deploy_service  #call deploy_service function
-#else REDEPLOYMENT=false
+	deploy_service  
 fi
-
-# verfify existing deployments nr for this particular env
-# use only repo name for pattern
-# probably a problem when pattern conflicts - eg cake3  & cake3php : pattern for cake3 might generate false conditional results - 5 cake3php deployments & 1 cake3 means no new cake3 service deployment even max nr it's not reached
 
 EXISTING_DEPLOYMENTS=`ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker ps  | grep -i "${LW_REPO_NAME}" | wc -l" `
 
@@ -265,15 +202,11 @@ if [ ${EXISTING_DEPLOYMENTS} -ge ${MAX_DEPLOYMENTS_NR} ]; then
 	RCMD="${RCMD} ${SSH_USER}@${DEPLOYMENT_SERVER} "
 	REM='" sudo docker stop '
 	RCMD="${RCMD} $REM ${OLDEST_DATABASE_CONTAINER} ${OLDEST_CONTAINER} "'"'
-#	echo "ssh -o "StrictHostKeyChecking no" -i  /tmp/sshkey.pem ${SSH_USER}@${DEPLOYMENT_SERVER} " sudo docker stop ${OLDEST_CONTAINER}"
         echo $RCMD | bash
 	sleep 12
-#	echo "Please cleanup environment manually before redeploy"
 	
-	deploy_service  #call deploy_service function
-#	exit -1
+	deploy_service  
 else 
 	echo "Deploying service ..."
-	deploy_service  #call deploy_service function
+	deploy_service  
 fi
-#
